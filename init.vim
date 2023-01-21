e6642de5bd72b639116f05bc121b0f0ad408ab90
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2023-01-21 18:05:17 +0700

    initial commit

---
+++ init.vim
@@ -0,0 +1,16 @@
+set relativenumber
+set number
+set hlsearch
+set autoindent
+set wildmode=longest,list
+filetype plugin indent on
+set tabstop=4
+set expandtab
+set shiftwidth=4
+set softtabstop=4
+set ignorecase
+set smartcase
+lua require('plugins')
+lua require('lsp')
+lua require('custom')
+colorscheme nord
---
+++ lua/plugins.lua
@@ -0,0 +1,52 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+
+    use {
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
+    use 'tpope/vim-surround'
+    use 'wakatime/vim-wakatime'
+    use 'tpope/vim-fugitive'
+    use 'arcticicestudio/nord-vim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+end)
