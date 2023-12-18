93ec3e5805e635f15d2eb33dc701b52644ea2b96
Muhammad Faizud Daroin <faizud.daroin@formulatrix.com>
2023-12-18 10:04:47 +0700

    fix: symbols outline icons

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,7 +6,7 @@ require("custom.dap")
 require("custom.rt")
 require("custom.options")
 require("toggleterm").setup()
-require("symbols-outline").setup()
+require("custom.symbols-outline")
 require("git-conflict").setup()
 require("catppuccin").setup({
     -- transparent_background = true,
