feaf9228a2131c17ea1b5c13c00f307e0cda1a8a
Muhammad Faizud Daroin <faizud.daroin@formulatrix.com>
2024-01-18 22:50:29 +0700

    fix: update lsp zero setup

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -17,7 +17,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 -- cmp_mappings['<Tab>'] = nil
 -- cmp_mappings['<S-Tab>'] = nil
 
-lsp.setup_nvim_cmp({
+cmp.setup({
     mapping = cmp_mappings
 })
 
