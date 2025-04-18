a2bba17f265c79d93d60eb910559bbefa9f65367
Muhammad Faizud Daroin <faizud.daroin@formulatrix.com>
2025-04-18 15:28:46 +0700

    feat: neotest and neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,15 @@
 return {
     { 'tpope/vim-fugitive' },
     { 'akinsho/git-conflict.nvim' },
-    { 'f-person/git-blame.nvim' }
+    { 'f-person/git-blame.nvim' },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+    }
 }
